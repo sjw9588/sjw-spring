95a6d1a4a0553d23e2ed167a87df9f45fa398d96
sjw <sjw9588@naver.com>
2019-03-24 04:44:14 -0700

    add

---
+++ src/main/java/com/sjw/jdbc/spring/Greeter.java
@@ -0,0 +1,11 @@
+package com.sjw.jdbc.spring;
+
+public class Greeter {
+	public Greeter() {
+		System.out.println("Greeter instance created.");
+		}
+		
+		public String greet() {
+			return "Hello, World!";
+}
+}
